554e080e6145190c4ceb439283b072286ff5583a
Safey Halim <safey.allah@gmail.com>
2022-01-17 07:48:53 +0100

    gitignore, Pipfile

---
+++ aa.py
@@ -0,0 +1,11 @@
+# AutoEncoders Movie Recommender
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn # PyTorch neural network module
+import torch.nn.parallel # PyTorch neural network parallel computing module
+import torch.optim as optim # PyTroch optimizer
+import torch.utils.data # PyTorch utilities
+from torch.autograd import Variable # PyTorch Gradient Descent module
